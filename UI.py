27181116efb2d6484ac7dc94cab900fb11116f2f
MuffinJets <hennkensk@gmail.com>
2022-05-17 15:37:19 -0500

    New savestates + added random seed in UI

--- UI.py
+++ UI.py
@@ -2,21 +2,22 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 import hashlib
+import random
 
 
 # Create a variable named root that holds/controls all tkinter functions
 root = tk.Tk()
 root.title('Attack of the Rhynocs Randomizer')
 root.grid()
-root.geometry('600x70')
+root.geometry('450x70')
 root.iconbitmap("icon.ico")
 
 frame = tk.Frame(root)
-frame.grid(column=0, row=1)
+frame.grid(columnspan=4, row=1)  # Don't touch this line of code ever :gun_emoji:
 
 correctROMBool = 0
 ROM = 0
-seed = 0
+seedEntryBox = tk.StringVar()
 settingsFlags = 0
 quickEscapeStart = tk.IntVar()
 generateSpoilerLog = tk.IntVar()
@@ -49,35 +50,43 @@ def loadROM():
         correctROMBool == 0
         print("correctROMBool = ", correctROMBool)
 
-
-
         
 
 
+def generateGame():
+    # print filepath of rom
+    print("ROM = ",ROM)
+    # grab seed value inputted by user, turn spinbox value into an integer
+    seed = int(seedEntryBox.get())
+    # "Is this the correct ROM?"
+    global correctROMBool
+    if correctROMBool == 1:
+        print("Generating Seed... (Currently no randomization functions written.)")
+        print("Seed Inputted =",seed)
+        if seed == 0:
+            seed = random.randrange(0,100000,1)
+            print("Random Seed = ", seed)
 
 
-def generateSeed():
-        print("ROM = ",ROM)
-        global correctROMBool
-        if correctROMBool == 1:
-            print("Generating Seed... (Currently no randomization functions written.)")
-        else:
-            print("Please open Spyro: Attack of the Rhynocs (U) ROM file.")
+
+
+
+
+
+        elif seed != 0:
+            print("Manual Seed = ", seed)
+    else:
+        print("Please open Spyro: Attack of the Rhynocs (U) ROM file.")
         
 
 
+openROMButton = tk.Button(root, text="Open ROM", fg="white", bg="#263D42", command=loadROM)
+seedEntryLabel = tk.Label(root, text="Seed(Leave 0 for random):")
+seedEntryBox = Spinbox(root, from_= 0, to = 99999, wrap=True)
+generateSeedButton = tk.Button(root, text="Generate Game", padx=10, fg="white", bg="#263D42", command=generateGame)
 
 quickEscapeStartCheck = tk.Checkbutton(frame, text="Start with Quick Escape", variable=quickEscapeStart, onvalue=1, offvalue=0)
 generateSpoilerLogCheck = tk.Checkbutton(frame, text="Generate Spoiler Log", variable=generateSpoilerLog, onvalue=1, offvalue=0)
-
-
-
-
-openROMButton = tk.Button(root, text="Open ROM", padx=10, pady=0, fg="white", bg="#263D42", command=loadROM)
-seedEntryLabel = tk.Label(root, text="Seed(Leave empty or 0 for random):")
-seedEntryBox = tk.Entry(root)
-generateSeedButton = tk.Button(root, text="Generate Game", padx=10, pady=0, fg="white", bg="#263D42", command=generateSeed)
-
 
 
 
@@ -86,8 +95,8 @@ seedEntryLabel.grid(column=1, row=0)
 seedEntryBox.grid(column=2, row=0)
 generateSeedButton.grid(column=3, row=0)
 
-quickEscapeStartCheck.grid(column=0, row=0)
-generateSpoilerLogCheck.grid(column=0, row=1)
+quickEscapeStartCheck.grid(column=0, row=1, padx=0)
+generateSpoilerLogCheck.grid(column=2, row=1, padx=0)
 
 
 
